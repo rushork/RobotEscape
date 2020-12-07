96f20f72fc590dfa442e293b1a2440c38996865a
Alexander Morris <73103701+rushork@users.noreply.github.com>
2020-12-07 04:40:48 +0000

    Attempted levels

---
+++ Assets/Scripts/levels/CompletedScript.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompletedScript : MonoBehaviour {
+    public bool isCompleted = false;
+}
---
+++ Assets/Scripts/menus/LevelCompletedScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCompletedScript : MonoBehaviour {
+    
+    public int currentLevel = 1;
+    public int numOfLevels = 2;
+    public GameObject[] levels;
+    private ArrayList levelCompleted = new ArrayList();
+
+    void Update() {
+        LevelUpdate();
+    }
+
+    void updateLevel() {
+        currentLevel += 1;
+    }
+
+    void LevelUpdate() {
+        for (int i = 0; i < numOfLevels; i++) {
+            if (i > currentLevel-1) {
+                levels[i].SetActive(false);
+            } else {
+                levels[i].SetActive(true);
+            }
+        }
+    }
+
+    void ResetCurrentLevel() {
+        currentLevel = 1;
+    }
+
+}
