9ab6ce9779d8b45862cfc63b76bb241bb573b5e0
Alexander Morris <73103701+rushork@users.noreply.github.com>
2021-03-29 23:25:29 +0800

    LEVEL fix?

--- Assets/Scripts/menus/LevelCompletedScript.cs
+++ Assets/Scripts/menus/LevelCompletedScript.cs
@@ -5,28 +5,29 @@ using UnityEngine.UI;
 
 public class LevelCompletedScript : MonoBehaviour {
     public GameObject[] levels;
-    private int numOfLevels = 2;
+    private int numOfLevels = 3;
     private static LevelSelectorScript[] level;
     public Text testText;
     
     void Start() {
         level = FindObjectsOfType<LevelSelectorScript>();
-
     }
 
-    void Update() {
+    void FixedUpdate() {
         testText.text = "Current Level: " + StaticLevelScript.currentLevel.ToString();
         LevelUpdate();
     }
 
     void LevelUpdate() {
-        for (int i = 0; i < numOfLevels; i++) {
+        /*
+        for(int i = 0; i < numOfLevels; i++) {
             if (i > StaticLevelScript.currentLevel-1) {
                 levels[i].SetActive(false);
             } else {
                 levels[i].SetActive(true);
             }
         }
+        */
     }
 
     void ResetCurrentLevel() {
